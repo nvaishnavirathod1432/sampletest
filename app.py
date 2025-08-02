84f45e60db3a0db9bd22fd6629e796871f08af5c
nvaishnavirathod0812 <nvaishnavirathod1432@gmail.com>
2025-08-02 12:39:20 +0530

    Add app.run to start Flask server

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route("/")
 def index():
     return "Hello"
+
 @app.route("/home")
 def home():
     return "Home Page"
-if __name__=="main_":
-    app.run(host='0.0.0.0',port=1000)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=1000)
